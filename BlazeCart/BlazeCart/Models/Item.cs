2a73d47af3ebb22fa11755d452b2b38a085d9ae1
Aleksandra Kondratjeva <89353862+artnaxel@users.noreply.github.com>
2022-09-30 13:15:10 +0300

    Login Page UI, draft Item and Cart models
    Added styles for buttons, register and login fields.

---
+++ BlazeCart/BlazeCart/Models/Item.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazeCart.Models
+{
+    internal class Item
+    {
+        [Key]
+        public int ItemId { get; set; }
+        public string CartId { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+    }
+}
