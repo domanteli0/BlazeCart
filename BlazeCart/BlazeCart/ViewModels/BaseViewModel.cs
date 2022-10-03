ef6764c825642273cc53865c5ddc28d926f394a8
GabrieleCiurlionyte <ciurlionyte.gabriele@gmail.com>
2022-10-03 10:25:05 +0300

    Created Error page, started MVVM

---
+++ BlazeCart/BlazeCart/ViewModels/BaseViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+
+namespace BlazeCart.ViewModels;
+
+
+
+public class BaseViewModel : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    public void OnPropertyChanged([CallerMemberName]string name = null) =>
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+}
