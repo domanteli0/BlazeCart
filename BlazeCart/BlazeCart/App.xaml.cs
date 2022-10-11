d3af2264fbe6a9bf65662122876f928ece3128d8
GabrieleCiurlionyte <ciurlionyte.gabriele@gmail.com>
2022-10-11 13:40:31 +0300

    Single Item detail page created

--- BlazeCart/BlazeCart/App.xaml.cs
+++ BlazeCart/BlazeCart/App.xaml.cs
@@ -9,8 +9,7 @@ public partial class App : Application
 	public App()
 	{
 		InitializeComponent();
-
-		//MainPage = new AppShell();
+        //MainPage = new AppShell();
 		MainPage = new NavigationPage(new WelcomePage1());
 	}
 }
---
+++ BlazeCart/BlazeCart/Views/ItemPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace BlazeCart.Views;
+
+public partial class ItemPage : ContentPage
+{
+	public ItemPage()
+	{
+		InitializeComponent();
+	}
+}
