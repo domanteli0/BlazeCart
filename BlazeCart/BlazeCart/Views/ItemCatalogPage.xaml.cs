bbadcc5e59dcc87e11884f559bbaba38d4015201
GabrieleCiurlionyte <ciurlionyte.gabriele@gmail.com>
2022-10-17 00:25:10 +0300

    Implemented a slider UI in ItemCatalogPage
    Slider will be used to filter price

--- BlazeCart/BlazeCart/Views/ItemCatalogPage.xaml.cs
+++ BlazeCart/BlazeCart/Views/ItemCatalogPage.xaml.cs
@@ -2,6 +2,8 @@ using Android.App.AppSearch;
 using BlazeCart.Models;
 using BlazeCart.Services;
 using BlazeCart.ViewModels;
+using Syncfusion.Maui.Sliders;
+
 namespace BlazeCart.Views;
 
 public partial class ItemCatalogPage : ContentPage
@@ -12,5 +14,14 @@ public partial class ItemCatalogPage : ContentPage
         BindingContext = vm;
     }
 
-    
+
+    private void OnValueChangeEnd(object sender, EventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void OnLabelCreated(object sender, SliderLabelCreatedEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
 }
