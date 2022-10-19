6e5503e274099112493e84c0e2e6995436eac1b5
Domanteli0 <domantelio@proton.me>
2022-10-19 19:55:18 +0300

    Created a rudementary barbora-scraper wraper

---
+++ BlazeCart/Scraper/BarboraScraperWrapper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Scraper
+{
+    public class BarboraScraperWrapper : Scraper
+    {
+        public BarboraScraperWrapper() : base() { }
+
+        /// <summary>
+        /// The file will be saved to $PROJECT_LOCATION/BlazeCart/Scraper/BarboraScraper/test.json
+        /// </summary>
+        // TODO?: Serialize data
+        public override void scrape()
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo()
+            {
+                WorkingDirectory = "../../../BarboraScraper/BarboraScraper/",
+                FileName = "scrapy",
+                Arguments = "crawl barbora -o test.json",
+            };
+            Process proc = new Process()
+            {
+                StartInfo = startInfo,
+            };
+            proc.Start();
+        }
+    }
+}
+
