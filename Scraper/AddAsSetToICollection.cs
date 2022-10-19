d1413053d9018d00c1169413e107ad8e11b5e3c2
Macbook Pro 2020 <macbookpro2020@Macbooks-MacBook-Pro.local>
2022-10-19 04:22:20 +0300

    Refactored the code a bit + bug fix Separated classes into files No more duplicates of products

---
+++ Scraper/AddAsSetToICollection.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Scraper
+{
+    static public class AddAsSetToICollection
+    {
+        /// <summary>
+        /// Checks if an object with the equal property (Determined by Equals() method) exists,
+        /// if not elem is added, otherthise it is not
+        /// NOTE: This method does not check if specified property is valid and exists
+        /// if it does not an exception will be thrown
+        /// NOTE: elem should not be null otherwise an exeption will be thrown
+        /// </summary>
+        public static void AddAsSetByProperty<T>(this ICollection<T> col, T elem, string property)
+        {
+            if (!col.Any(l => {
+                return l.GetType().GetProperty(property)!.GetValue(l)!.Equals(
+                    elem.GetType().GetProperty(property)!.GetValue(elem)
+                    );
+            })
+            )
+                col.Add(elem);
+        }
+    }
+}
+
---
+++ Scraper/Scraper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Scraper
+{
+    public abstract class Scraper
+    {
+        // All categories, including those who have subcategories
+        public List<Category> AllCategories { get; private protected set; } = new List<Category>();
+        // Only categories, which don't have any subcategories
+        public List<Category> Categories { get; private protected set; } = new List<Category>();
+        public List<Store> Stores { get; private protected set; } = new List<Store>();
+        public List<Product> Products { get; private protected set; } = new List<Product>();
+
+        protected HttpClient httpClient = new HttpClient();
+
+        public Scraper() { }
+
+        // re-fetches all data
+        abstract public void scrape();
+    }
+}
+
