04a17b6855ed4a126d478694fd110d4512768aff
Domanteli0 <domantelio@proton.me>
2022-10-19 20:00:57 +0300

    Implemented 'UpdateOrAddByProperty' and added test for this method

--- BlazeCart/Scraper/CollectionExtentions.cs
+++ BlazeCart/Scraper/CollectionExtentions.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 namespace Scraper
 {
-    static public class AddAsSetToICollection
+    static public class CollectionExtentions
     {
         /// <summary>
         /// Checks if an object with the equal property (Determined by Equals() method) exists,
@@ -12,13 +12,34 @@ namespace Scraper
         /// </summary>
         public static void AddAsSetByProperty<T>(this ICollection<T> col, T elem, string property)
         {
-            if (!col.Any(l => {
-                return l.GetType().GetProperty(property)!.GetValue(l)!.Equals(
+            if (!col.Any(e => {
+                return e.GetType().GetProperty(property)!.GetValue(e)!.Equals(
                     elem.GetType().GetProperty(property)!.GetValue(elem)
                     );
             })
             )
                 col.Add(elem);
+        }
+
+        /// <summary>
+        /// Finds the first element with matching property (Determined by Equals() method)
+        /// and replaces it with elem.
+        /// If no property mathcing element is found then it is just added to the collection.
+        /// </summary>
+        public static void UpdateOrAddByProperty<T>(this List<T> col, T elem, string property)
+        {
+            var temp = col.Find(e =>
+            {
+                return e.GetType().GetProperty(property)!.GetValue(e)!.Equals(
+                    elem.GetType().GetProperty(property)!.GetValue(elem));
+            });
+
+            if (temp is not null)
+            {
+                col.Remove(temp);
+            }
+
+            col.Add(elem);
         }
     }
 }
--- BlazeCart/Tests/UnitTest1.cs
+++ BlazeCart/Tests/UnitTest1.cs
@@ -1,10 +1,35 @@
 ﻿using Scraper;
+using System.Collections.Generic;
+using static Scraper.CollectionExtentions;
 
 namespace Tests;
 
 [TestClass]
 public class UnitTest1
 {
+
+    private class A
+    {
+        public int B { get; set; }
+        public int C { get; set; }
+    }
+
+    [TestMethod]
+    public void UpdateOrAddByPropertyTest()
+    {
+        var a0 = new A() { B = 1, C = 2, };
+        var a1 = new A() { B = 3, C = 4, };
+        var a2 = new A() { B = 1, C = 40, };
+
+        var list = new List<A>(new A[] { a0, a1 });
+
+        list.UpdateOrAddByProperty(a2, "B");
+
+        Assert.AreEqual(list.Contains(a0), false);
+        Assert.AreEqual(list.Contains(a1), true);
+        Assert.AreEqual(list.Contains(a2), true);
+    }
+
     [TestMethod]
     public void IKIDuplicateTest()
     {
@@ -21,5 +46,14 @@ public class UnitTest1
             Assert.AreEqual(IkiScraper.AllCategories.FindAll(s => s.InternalID.Equals(cat.InternalID)).Count, 1, "Duplicate found in AllCategories");
         foreach (var item in IkiScraper.Items)
             Assert.AreEqual(IkiScraper.Items.FindAll(s => s.InternalID.Equals(item.InternalID)).Count, 1, "Duplicate found in Products");
+
+        foreach (var store in IkiScraper.Stores)
+            Assert.AreEqual(IkiScraper.Stores.FindAll(s => s.Equals(store)).Count, 1, "Duplicate found  in Stores");
+        foreach (var cat in IkiScraper.Categories)
+            Assert.AreEqual(IkiScraper.Categories.FindAll(s => s.Equals(cat)).Count, 1, "Duplicate found in Categories");
+        foreach (var cat in IkiScraper.AllCategories)
+            Assert.AreEqual(IkiScraper.AllCategories.FindAll(s => s.Equals(cat)).Count, 1, "Duplicate found in AllCategories");
+        foreach (var item in IkiScraper.Items)
+            Assert.AreEqual(IkiScraper.Items.FindAll(s => s.Equals(item)).Count, 1, "Duplicate found in Products");
     }
 }
