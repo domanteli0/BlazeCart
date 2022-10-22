5181c9f9dae883e3f5d53b723b91ddcf091199d7
Domanteli0 <domantelio@proton.me>
2022-10-22 18:44:58 +0300

    Init commit for REST API Also moved 'Store', 'Category' and 'Item' structs and classes to 'Models' project

---
+++ BlazeCart/Api/Controllers/ItemController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+//using Models;
+using Scraper;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Api.Controllers
+{
+    [Route("api/items")]
+    public class ItemController : Controller
+    {
+        // GET: api/values
+        [HttpGet(Name = "api/items")]
+        public IEnumerable<Item> Get()
+        {
+            // TODO: Query the database once DB is implemented
+            return (IEnumerable<Item>) (new IKIScraper()).Items;
+            //return new List<string>();
+        }
+    }
+}
+
