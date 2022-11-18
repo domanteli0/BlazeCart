5bd197e3c963eb58101eb08812e70edf7363332b
Domanteli0 <domantelio@proton.me>
2022-11-18 14:37:04 +0200

    Downgraded `Microsoft.Extensions.Configuration` to version 6.0.0 This solves the missing `Microsoft.Extensions.Configuration.Abstractions` error

--- BlazeCart/ScraperFunction/ScraperFunction.cs
+++ BlazeCart/ScraperFunction/ScraperFunction.cs
@@ -4,7 +4,6 @@ using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
 //using Microsoft.Extensions.Logging.Abstractions;
 
-using Models;
 using Scraper;
 using DB;
 using Microsoft.EntityFrameworkCore;
