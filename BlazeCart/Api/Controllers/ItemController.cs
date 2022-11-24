164c521e33ff348e10c45b3ab64bb5d7a0525cad
Aleksandra Kondratjeva <89353862+artnaxel@users.noreply.github.com>
2022-11-24 16:21:19 +0200

    Remove unnecessary attributes `ItemController.cs`

--- BlazeCart/Api/Controllers/ItemController.cs
+++ BlazeCart/Api/Controllers/ItemController.cs
@@ -25,7 +25,7 @@ namespace Api.Controllers
 
         [HttpGet("{index}/{count}")]
         [ProducesResponseType(200, Type = typeof(IEnumerable<Item>))]
-        public async Task<ActionResult<IEnumerable<Item>>> GetRangeOfItems([FromQuery] int index, [FromQuery] int count = 10)
+        public async Task<ActionResult<IEnumerable<Item>>> GetRangeOfItems(int index, int count)
         {
             var items = await _itemRepository.GetRangeOfItemsAsync(index, count);
             if (!ModelState.IsValid)
