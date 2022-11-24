b7627af77c242aacdb2239317d0d272bac2e2a4d
Domanteli0 <domantelio@proton.me>
2022-11-24 23:01:34 +0200

    Porter `UpdateOrAddByPropertyTest` to XUnit

--- BlazeCart/Tests/UnitTest1.cs
+++ BlazeCart/Tests/UnitTest1.cs
@@ -6,29 +6,6 @@ namespace Tests;
 [TestClass]
 public class UnitTest1
 {
-
-    private class A
-    {
-        public int B { get; set; }
-        public int C { get; set; }
-    }
-
-    [TestMethod]
-    public void UpdateOrAddByPropertyTest()
-    {
-        var a0 = new A() { B = 1, C = 2, };
-        var a1 = new A() { B = 3, C = 4, };
-        var a2 = new A() { B = 1, C = 40, };
-
-        var list = new List<A>(new A[] { a0, a1 });
-
-        list.UpdateOrAddByProperty(a2, "B");
-
-        Assert.AreEqual(list.Contains(a0), false);
-        Assert.AreEqual(list.Contains(a1), true);
-        Assert.AreEqual(list.Contains(a2), true);
-    }
-
     [TestMethod()]
     public async Task BarboraDuplicateTest()
     {
---
+++ BlazeCart/Tests1/Common/CollectionExtensionsTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Common;
+using FakeItEasy;
+
+namespace Tests1.Common
+{
+	public class CollectionExtensionsTest
+	{
+        private class A
+        {
+            public int B { get; set; }
+            public int C { get; set; }
+        }
+
+        public CollectionExtensionsTest()
+		{
+
+        }
+
+        [Fact]
+        public void UpdateOrAddByPropertyTest()
+        {
+            var a0 = new A() { B = 1, C = 2, };
+            var a1 = new A() { B = 3, C = 4, };
+            var a2 = new A() { B = 1, C = 40, };
+
+            var list = new List<A>(new A[] { a0, a1 });
+
+            list.UpdateOrAddByProperty(a2, "B");
+
+            Assert.DoesNotContain(a0, list);
+            Assert.Contains(a1, list);
+            Assert.Contains(a2, list);
+        }
+
+    }
+}
+
