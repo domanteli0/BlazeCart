964176afb7d6df11116b7cc8f03aad682fb77e74
Domanteli0 <domantelio@proton.me>
2022-11-25 05:25:34 +0200

    Clean up

--- BlazeCart/Api/Program.cs
+++ BlazeCart/Api/Program.cs
@@ -1,15 +1,11 @@
 ﻿using DB;
 using System;
-//using System.Collections.Generic;
 using System.Configuration;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Api.Repositories;
 
-//using Microsoft.Extensions.DependencyInjection.Extensions;
-//using Microsoft.Extensions.DependencyInjection;
-//using Microsoft.Extensions.Logging;
-
+// https://stackoverflow.com/questions/63321774/deploying-asp-net-core-app-to-azure-fails-when-connecting-to-adding-the-databa
 namespace Api
 {
     public class Program
